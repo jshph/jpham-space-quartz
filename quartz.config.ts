657fd2a2072eef3347d144382f4b429d467354a9
Joshua Pham <joshua.pham@gmail.com>
2024-09-16 00:41:06 -0400

    evoc title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "jphams space",
+    pageTitle: "human-computer ouroborosis",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
